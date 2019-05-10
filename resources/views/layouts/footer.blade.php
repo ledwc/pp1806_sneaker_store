4d97880e055d2ee1a88ebec118a5f476d90b1dcf
ledwc <leduc.kma@gmail.com>
2019-05-10 20:46:19 +0700

    Add layout.

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,35 @@
+<footer class="page-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col s4">
+                <h5 class="deep-orange-text">Sneaker Store - 100% Authentic</h5>
+                <p class="orange-text text-darken-4">Địa Chỉ: Tầng 11 - Tòa nhà Handico - Hà Nội.</p>
+                <p class="orange-text text-darken-4">Email: leduc.kma@gmail.com</p>
+                <p class="orange-text text-darken-4">Hotline: 035 407 2020</p>
+            </div>
+            <div class="col offset-l1 s3">
+                <h5 class="deep-orange-text">Hướng dẫn</h5>
+                    <ul>
+                      <li><a class="orange-text text-darken-4" href="#!">Hướng dẫn mua hàng</a></li>
+                      <li><a class="orange-text text-darken-4" href="#!">Hướng dẫn thanh toán</a></li>
+                      <li><a class="orange-text text-darken-4" href="#!">Hướng dẫn giao nhận</a></li>
+                      <li><a class="orange-text text-darken-4" href="#!">Điều khoản dịch vụ</a></li>
+                    </ul>
+            </div>
+            <div class="col offset-l1 s3">
+                <h5 class="deep-orange-text">Chính sách</h5>
+                <ul>
+                  <li><a class="orange-text text-darken-4" href="#!">Chính sách bảo mật</a></li>
+                  <li><a class="orange-text text-darken-4" href="#!">Chính sách vận chuyển</a></li>
+                  <li><a class="orange-text text-darken-4" href="#!">Chính sách đổi trả</a></li>
+                  <li><a class="orange-text text-darken-4" href="#!">Quy định sử dụng </a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            <p class="orange-text text-darken-4">© 2019 Copyright LeDuc</p>
+        </div>
+    </div>
+</footer>
